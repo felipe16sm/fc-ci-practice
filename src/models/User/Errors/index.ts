0b10a6353c945ad97786f780db8ae83cd54a026e
Felipe Sobrinho Molina <felipe16sm@gmail.com>
2023-07-09 15:40:24 -0300

    feat: add User Errors

---
+++ src/models/User/Errors/index.ts
@@ -0,0 +1,8 @@
+class Errors {
+  static INVALID_ID = "Invalid id. Id must be an UUID";
+  static NO_EMPTY_ID = "Id cannot be empty";
+  static NO_EMPTY_USERNAME = "Username cannot be empty";
+  static NO_EMPTY_PASSWORD = "Password cannot be empty";
+}
+
+export default Errors;
